0ee3b220b1f49bb7086e3de9957cba97642c09b6
drs-t0m0 <drs-tomo@hotmail.co.jp>
2021-10-13 11:21:17 +0900

    add database

--- src/main.py
+++ src/main.py
@@ -3,8 +3,12 @@ from fastapi import FastAPI
 
 from src.api.api import router
 from src.core.config import APIConfigurations
+from src.db import initialize
+from src.db.database import engine
 
 logger = getLogger(__name__)
+
+initialize.initialize_table(engine=engine, checkfirst=True)
 
 app = FastAPI(
     title=APIConfigurations.title,
